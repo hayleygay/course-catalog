dc352d4826f75e7ed61d4b8063ce16e09359f894
hayleygay <hgay1@vcd-ch204-16.eastern.ewu.edu>
2016-11-08 14:33:20 -0800

    Version 1.5
    Added CSS styling and grid view JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-// YOUR SCRIPTS
+// GRID VIEW
+
+$('#grid').click( function({
+    // REMOVE ONLY 'LIST'
+    $('.cd-gallery').removeClass('list')
+    // ADD ONLY 'GRID'
+    $('.cd-gallery').addClass('grid')
+    
+    console.log('OK');
+}) );
+                 
+// LIST VIEW
+                 
+    $('#list').click( function({
+    // REMOVE ONLY 'GRID'
+    $('.cd-gallery').removeClass('grid')
+    // ADD ONLY 'LIST'
+    $('.cd-gallery').addClass('list')
+    
+    console.log('OK');
+}) );
